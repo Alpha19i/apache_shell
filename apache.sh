64f6dbabfd59e091b48bd9b40dd2ee79516ebfec
Camilo Italo <alpha19ci@gmail.com>
2023-02-27 18:26:38 -0300

    script: baixa e copia o site

--- apache.sh
+++ apache.sh
@@ -5,3 +5,10 @@ apt-get update
 apt-get upgrade -y
 apt-get install apache2 -y
 apt-get install unzip -y
+
+echo "Baixando e Copiando"
+cd /tmp
+wget https://github.com/Alpha19i/calculadoraSimples/archive/refs/heads/main.zip
+unzip main.zip
+cd calculadoraSimples-main
+cp -R * /var/www/html/
